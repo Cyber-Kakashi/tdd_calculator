ff96eb49577fd7840af7f5c15a221956a0066ae0
vishvesh choudhary <vishvesh432@gmail.com>
2024-12-07 18:36:26 +0530

    step7 and 8: updated the function to handle multiple custom delimiters

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,24 +1,33 @@
 "use strict";
 
 export function calculator(equation: string): number {
-    const delimiterRegex = /^\/\/([^\\n]+)\n/;
+    let delimiterRegex;
     const negativeNumbers: number[] = [];
     let sum = 0;
     if (!equation.length) return sum;
-    let delimiter = ',';
+    const defaultDelimiter = ',';
 
-    // supporting custom delimiters
-    const match = equation.match(delimiterRegex);
-    if (match) {
-        delimiter = match[1];
+    // custom delimiters
+    let delimiters = getDelimiters(equation);
+    if (delimiters) {
+        try {
+            const specialCharacters = /[.*+?^=!:${}()|\[\]\/\\]/g;
+            delimiters = delimiters.map(item => item.replace(specialCharacters, '\\$&'));
+            delimiterRegex = new RegExp(delimiters.join('|'), 'g');
+            console.log('delimiters: ', delimiterRegex);
+            equation = equation.replace(delimiterRegex, defaultDelimiter)
+            console.log('equations: ', equation);
+    
+        } catch (e) {}
     }
+
 
     // handling the '\n' in the equation string
     // replacing '\n' with ',' so that it is used as a delimiter
-    equation = equation.replace('\n', delimiter)
+    equation = equation.replace('\n', defaultDelimiter)
 
     // assuming the ',' delimiter for now
-    const parsed_equation = equation.split(delimiter);
+    const parsed_equation = equation.split(defaultDelimiter);
 
     // validating the parsed_equation
     // Making sure we have only numeric values in the parsed_equation array
@@ -48,3 +57,20 @@ export function calculator(equation: string): number {
 
     return sum;
 }
+
+
+function getDelimiters(equation: string): string[] | null {
+    // match the delimiters pattern: //[*][%][delim3]
+    const delimiterPattern = /\[([^\]]+)\]/g;
+    
+    // match all the delimiters inside square brackets
+    const match = equation.match(delimiterPattern);
+  
+    if (match) {
+        return match.map(item => item.replace(/[\[\]]/g, ''));
+    }
+  
+    return null;
+  }
+  
+  
--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -3,147 +3,205 @@ import { calculator } from '../src/calculator';
 const TEST_CASES = [
     // STEP 1: test cases
     {
+        count: 1,
         message: 'Sum for EMPTY string',
         case: '',
         result: 0
     },
     {
+        count: 2,
         message: 'Sum for a single number-string',
         case: '1',
         result: 1
     },
     {
+        count: 3,
         message: 'Sum for a single number-string (multiple digit (2))',
         case: '12',
         result: 12
     },
     {
+        count: 4,
         message: 'Sum for a single number-string (multiple digit (3))',
         case: '123',
         result: 123
     },
     {
+        count: 5,
         message: 'Sum for a two numbers-string',
         case: '1,2',
         result: 3
     },
     {
+        count: 6,
         message: 'Sum for a two numbers-string (single-digit, multi-digit)',
         case: '1,12',
         result: 13
     },
     {
+        count: 7,
         message: 'Sum for a two numbers-string (multi-digit, single-digit)',
         case: '1,23',
         result: 24
     },
     // STEP 2: test cases
     {
+        count: 8,
         message: 'Sum for a N numbers-string',
         case: '1,2,3,4,5',
         result: 15
     },
     {
+        count: 9,
         message: 'Sum for a N numbers-string',
         case: '1,22,134,424,5',
         result: 586
     },
     // STEP 3: test cases
     {
+        count: 10,
         message: 'Allow \\n instead of commas',
         case: '1,22\n134,424,5',
         result: 586
     },
     {
+        count: 11,
         message: 'Allow consecutive \\n instead of commas',
         case: '1,22\n134,\n\n424,5',
         result: 586
     },
     {
+        count: 12,
         message: 'Allow multiple consecutive \\n instead of commas',
         case: '1,22\n134,424\n\n\n\n\n,5',
         result: 586
     },
-    // STEP 4: Allow custom delimiters
+    // STEP 4: Allow custom and multiple delimiters
     {
-        message: '//;\\nAllow \\n instead of commas',
-        case: '//;\n1;22;134;424;5',
+        count: 13,
+        message: '//[;]\\nAllow \\n instead of commas',
+        case: '//[;]\n1;22;134;424;5',
         result: 586
     },
     {
-        message: '// \\nAllow \\n instead of commas',
-        case: '// \n1 22 134 424 5',
+        count: 14,
+        message: '//[ ]\\nAllow \\n instead of commas',
+        case: '//[ ]\n1 22 134 424 5',
+        result: 586
+    },
+    {
+        count: 15,
+        message: '//[%%%%]\\nAllow \\n instead of commas',
+        case: '//[%%%%]\n1%%%%22%%%%134%%%%424%%%%5',
+        result: 586
+    },
+    {
+        count: 16,
+        message: '//[---]\\nAllow \\n instead of commas',
+        case: '//[---]\n1---22---134---424---5',
+        result: 586
+    },
+    {
+        count: 17,
+        message: '//[@@@@][;]\\nAllow \\n instead of commas',
+        case: '//[@@@@][;]\n1;22;134;424;5',
+        result: 586
+    },
+    {
+        count: 18,
+        message: '//[@@@@][;]\\nAllow \\n instead of commas',
+        case: '//[@@@@][;]\n1;22@@@@134;424;5',
+        result: 586
+    },
+    {
+        count: 19,
+        message: '//[@@**][;]\\nAllow \\n instead of commas',
+        case: '//[@@**][;]\n1;22@@**134;424;5',
         result: 586
     },
     // STEP 5: Do NOTE allow negative numbers
     {
+        count: 20,
         message: 'Do not allow negative numbers',
         case: '-1',
         error: 'Negative numbers not allowed. Found: [-1]'
     },
     {
+        count: 21,
         message: 'Do not allow negative numbers',
         case: '12,-2',
         error: 'Negative numbers not allowed. Found: [-2]'
     },
     {
+        count: 22,
         message: 'Do not allow negative numbers',
         case: '-3,-2',
         error: 'Negative numbers not allowed. Found: [-3,-2]'
     },
     {
+        count: 23,
         message: 'Do not allow negative numbers',
         case: '-2,-3,-5,-2',
         error: 'Negative numbers not allowed. Found: [-2,-3,-5,-2]'
     },
     {
+        count: 24,
         message: 'Do not allow negative numbers',
-        case: '//;\n12;-2',
+        case: '//[;]\n12;-2',
         error: 'Negative numbers not allowed. Found: [-2]'
     },
     // STEP 6: Do NOT allow number above 1000 to be summed
     {
+        count: 25,
         message: 'Do NOT allow number above 1000',
         case: '1, 1000',
         result: 1001
     },
     {
+        count: 26,
         message: 'Do NOT allow number above 1000',
         case: '1,, 2, 1000',
         result: 1003
     },
     {
+        count: 27,
         message: 'Do NOT allow number above 1000',
         case: '1, 1001',
         result: 1
     },
     {
+        count: 28,
         message: 'Do NOT allow number above 1000',
         case: '2, 100230,3,4',
         result: 9
     },
     {
+        count: 29,
         message: 'Do NOT allow number above 1000',
         case: '1, 999',
         result: 1000
     },
     // EDGE CASES: specific to TypeScript or other outliers
     {
+        count: 30,
         message: 'Sum for a two numbers-string (multi-digit, multi-digit)',
         case: '12,23',
         result: 35
     },
     {
+        count: 31,
         message: '[EDGE CASE] non numeric values in string',
         case: 'sdf',
         result: 0
     },
     {
+        count: 32,
         message: '[EDGE CASE] non numeric values in string. Should only consider numbers for summation',
         case: '1,sdf',
         result: 1
     },
     {
+        count: 33,
         message: '[EDGE CASE] non numeric(spaces) values in string. Should only consider numbers for summation',
         case: '15, 2',
         result: 17
@@ -152,7 +210,7 @@ const TEST_CASES = [
 
 describe('calculator function', () => {
     TEST_CASES.forEach(t => {
-        it(t.message, () => {
+        it(t.count + ': ' + t.message, () => {
             if (t.error) {
                 expect(() => calculator(t.case)).toThrow(t.error);
             } else {
