ca14d6827cf332273aaaddc809e9051b8532cf3d
vishvesh choudhary <vishvesh432@gmail.com>
2024-12-07 15:06:45 +0530

    feat: tested the support a single number with multiple digits

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -5,8 +5,16 @@ describe('calculator function', () => {
     expect(calculator('')).toBe(0);
   });
 
-
   it('Sum for a single number string', () => {
     expect(calculator('1')).toBe(1);
+  });
+
+  it('Sum for a single number (multiple digit (2)) string', () => {
+    expect(calculator('12')).toBe(12);
+  });
+
+
+  it('Sum for a single number (multiple digit (3)) string', () => {
+    expect(calculator('123')).toBe(123);
   });
 });
