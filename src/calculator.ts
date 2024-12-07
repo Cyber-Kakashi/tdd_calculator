0c9129c07770f186b3f6f762a94df60f6ebf73e1
vishvesh choudhary <vishvesh432@gmail.com>
2024-12-07 15:04:14 +0530

    fix: updated the clculator function to handle one number from a string

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,7 @@
 export function calculator(equation: string): number {
     let sum = 0;
     if (!equation.length) return sum;
+
+    sum += +equation;
     return sum;
 }
--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -4,4 +4,9 @@ describe('calculator function', () => {
   it('Sum for EMPTY string', () => {
     expect(calculator('')).toBe(0);
   });
+
+
+  it('Sum for a single number string', () => {
+    expect(calculator('1')).toBe(1);
+  });
 });
